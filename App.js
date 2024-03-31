98ca28f42a53f231395707c9284c0e6be2ae119b
Aidan Gor <108840381+AidanGor@users.noreply.github.com>
2024-03-30 23:32:15 -0400

    Pushed initial prototype

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import { Canvas, Path } from "@shopify/react-native-skia";
 
-export default function App() {
+export default function Draw() {
+  const [paths, setPaths] = useState([]);
+
+  const pan = Gesture.Pan()
+    .onStart((g) => {
+      setPaths(prevPaths => {
+        const newPaths = [...prevPaths];
+        newPaths.push({
+          segments: [`M ${g.x} ${g.y}`], // Initialize segments with start point
+          color: "#06d6a0",
+        });
+        return newPaths;
+      });
+    })
+    .onUpdate((g) => {
+      setPaths(prevPaths => {
+        if (prevPaths.length === 0) return prevPaths; // Return if no paths are drawn yet
+        const index = prevPaths.length - 1;
+        const newPaths = [...prevPaths];
+        newPaths[index].segments.push(`L ${g.x} ${g.y}`); // Add line segment
+        return newPaths;
+      });
+    })
+    .minDistance(1);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureDetector gesture={pan}>
+        <View style={{ flex: 1, backgroundColor: "black" }}>
+          <Canvas style={{ flex: 1 }}>
+            {paths.map((p, index) => (
+              <Path
+                key={index}
+                path={p.segments.join(" ")}
+                strokeWidth={5}
+                style="stroke"
+                color={p.color}
+              />
+            ))}
+          </Canvas>
+        </View>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
